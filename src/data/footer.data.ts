a7afe2594b516c3faa3fb75e9b122d17ea1e2b15
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-03-26 01:36:04 +0530

    Added footer data section

---
+++ src/data/footer.data.ts
@@ -0,0 +1,48 @@
+export const footer = {
+    upperSection: {
+        companyDetails: {
+            name: 'HomeQuest',
+            mobile: '(907) 357-4333',
+            email: 'hello@homequest.com',
+            socialLinks: [
+                {
+                    name: 'Facebook',
+                    link: 'www.facebook.com'
+                },
+                {
+                    name: 'Linkedin',
+                    link: 'www.linkedin.com'
+                },
+                {
+                    name: 'Youtube',
+                    link: 'www.youtube.com'
+                },
+            ]
+        },
+        navigationLinks: [
+            {
+                name: 'Navigation',
+                links: ['Company', 'Properties', 'Services', 'Contact Us']
+            },
+            {
+                name: 'Resources',
+                links: ['Agent', 'Blog', 'How it works', 'License']
+            },
+            {
+                name: 'Office Location',
+                links: ['Emily Hattson', '940 Goldendale Dr, Wasilla, Alaska', '99654, USA']
+            },
+        ]
+    },
+    lowerSection: {
+        copyright: '© Homequest 2024 All right reserved',
+        termsConditions: {
+            name: 'Terms & Conditions',
+            link: '/terms-and-conditions',
+        },
+        privacyPolicy: {
+            name: 'Privacy Policy',
+            link: '/privacy-policy'
+        }
+    }
+}
