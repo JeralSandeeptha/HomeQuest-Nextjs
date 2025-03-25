2bdaaa89500f73678361b70a40492e5a3909eeaa
JeralSandeeptha <jeral.sandeeptha1@gmail.com>
2025-03-26 01:22:20 +0530

    Added some components and added not found page

---
+++ src/data/not-found.data.ts
@@ -0,0 +1,6 @@
+export const notFoundPage = {
+    title: 'Oops! Page Not Found',
+    description: 'It looks like the page you’re looking for doesn’t exist. Don’t worry, we’ll help you get back on track!',
+    buttonText: 'Go back to Homepage',
+    link: '/'
+}
